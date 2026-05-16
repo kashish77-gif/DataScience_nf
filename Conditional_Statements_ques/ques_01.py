c5e51adbd2cb5578f2dcca2737aa9f1342cc2cf6
Kashish77 <Kashishvashishtha7@gmail.com>
2026-05-16 23:39:13 +0530

    Added Conditiona _Statement_ques

---
+++ Conditional_Statements_ques/ques_01.py
@@ -0,0 +1,8 @@
+ #Write a program to check whether a year is a leap year or not
+
+year = int(input("Enter a year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(year, "is a Leap Year")
+else:
+    print(year, "is not a Leap Year")
